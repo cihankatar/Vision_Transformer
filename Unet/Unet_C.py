63a6dcebbadfc7ca16f367b79948fc4740b164f6
cihankatar <katarcihan@gmail.com>
2023-02-21 00:29:10 +0300

    new

--- Unet/Unet_C.py
+++ Unet/Unet_C.py
@@ -15,55 +15,60 @@ from torch.utils.data import Dataset
 from PIL import Image
 
 
-'''
-image=Image.open("train/images/cju0qkwl35piu0993l0dewei2.jpg")
-image.show()
-print(type(image))
-image=np.array(image)
-image=torch.from_numpy(image)
-print(image.shape)
-'''
-
-cwd = os.getcwd() 
-print("Current working directory:", cwd) 
-
-
-class dataset(Dataset):
-    def __init__(self,path,image_dir_list):
+class KVasir_dataset(Dataset):
+    def __init__(self,train_path,mask_path,image_dir_list,mask_dir_list):
         super().__init__()
-        self.path       = path
+        self.train_path      = train_path
         self.image_dir_list  = image_dir_list
-        self.images=[]
+        self.mask_path       = mask_path
+        self.mask_dir_list   = mask_dir_list
 
+        self.images=[]
+        self.masks=[]
 
     def getitems(self):        
         for idx in range(len(self.image_dir_list)):
             #if 'jpg' in self.image_dir_list:
-            image_dir = os.path.join(path,self.image_dir_list[idx])
+            image_dir = os.path.join(self.train_path,self.image_dir_list[idx])
             image=Image.open(image_dir)
             image=np.array(image)
             image = np.transpose(image, (2, 0, 1))
             image=torch.from_numpy(image)
             self.images.append(image)
-        return self.images
+        
+        for idx in range(len(self.image_dir_list)):
+            #if 'jpg' in self.image_dir_list:
+            mask_dir = os.path.join(self.mask_path,self.mask_dir_list[idx])
+            mask=Image.open(mask_dir)
+            mask=np.array(mask)
+            mask = np.transpose(mask, (2, 0, 1))
+            mask=torch.from_numpy(mask)
+            self.masks.append(mask)
+
+        return self.images, self.masks
     
     def len(self):
         return len(self.image_dir_list)
 
+def main():
+    train_im_path = "train/images"
+    images_dir_list = os.listdir(train_im_path) 
+    train_mask_path = "train/masks"
+    mask_dir_list = os.listdir(train_mask_path) 
 
-path = "train/images"
-images_dir_list = os.listdir(path) 
-print("Files and directories in '", path, "' :")  
-print('type:{},lengt:{}'.format(type(images_dir_list),len(images_dir_list)))
+    data=KVasir_dataset(train_im_path,train_mask_path,images_dir_list,mask_dir_list)
+    train_dataset = list(data.getitems())
+    train_dataset = next(iter(train_dataset))
 
-print(images_dir_list)
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=10,
+        shuffle=True,
+        num_workers=2
+    )
 
-Data=dataset(path,images_dir_list)
-train_dataset = Data.getitems()
-train_loader = DataLoader(
-    dataset=train_dataset,
-    batch_size=128,
-    shuffle=True,
-    num_workers=2
-)
+    for batch in train_loader:
+        x,y=batch
 
+if __name__ == '__main__':
+    main()
