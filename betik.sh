20184387182907697653c92f4b49caf8610f0e4b
cihankatar <katarcihan@gmail.com>
2023-04-01 16:59:06 +0300

    n

--- betik.sh
+++ betik.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-#SBATCH -p barbun           # Kuyruk adi: Uzerinde GPU olan kuyruk olmasina dikkat edin.
+#SBATCH -p akya-cuda        # Kuyruk adi: Uzerinde GPU olan kuyruk olmasina dikkat edin.
 #SBATCH -A ckatar           # Kullanici adi
 #SBATCH -J vision           # Gonderilen isin ismi
-#SBATCH -o sonuc.out        # Ciktinin yazilacagi dosya adi
+#SBATCH -o vision.out       # Ciktinin yazilacagi dosya adi
 #SBATCH --gres=gpu:1        # Her bir sunucuda kac GPU istiyorsunuz? Kumeleri kontrol edin.
 #SBATCH -N 1                # Gorev kac node'da calisacak?
 #SBATCH -n 1                # Ayni gorevden kac adet calistirilacak?
-#SBATCH --cpus-per-task 32  # Her bir gorev kac cekirdek kullanacak? Kumeleri kontrol edin.
-#SBATCH --time=0:15:00      # Sure siniri koyun.
+#SBATCH --cpus-per-task 10  # Her bir gorev kac cekirdek kullanacak? Kumeleri kontrol edin.
+#SBATCH --time=0:05:00      # Sure siniri koyun.
 
 eval "$(/truba/home/$USER/miniconda3/bin/conda shell.bash hook)"
 conda activate segmentation 
 cd Desktop/Github_Repo/Vision_Transformer
-python ViT_Paper_Replica.py
+python ViT.py
 exit
+
